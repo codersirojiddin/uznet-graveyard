2a29cefd1d14ba95747f75cb43a3a8e8a82d7cf4
Sirojiddin Yoqubjonov <codersirojiddin@gmail.com>
2026-08-06 21:16:55 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ export const metadata: Metadata = {
   title: "UzNet Graveyard — O'zbekiston internet olami qabristoni",
   description:
     "2000–2020 yillarda faoliyat yuritgan, keyinchalik yopilgan yoki sotib olingan afsonaviy UzNet loyihalarining interaktiv qabristoni.",
+  verification: {
+    google: "0ZD9S1o2maf53dKdwbYM9EjyBaIzo-aU7GDexaweITk",
+  },
 };
 
 export default function RootLayout({
